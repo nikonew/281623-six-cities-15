19535ad1dcfaf1922457a9a9999d4d1955419ae8
nikonew <spiritofadv@yandex.ru>
2024-02-23 22:10:25 +0300

    реализовал маршрутизацию и попроравил разметку

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './componets/app/app';
+import App from './app/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
