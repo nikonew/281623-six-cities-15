726f6a540029fc01c6a66110e25066809d0e11e9
nikonew <spiritofadv@yandex.ru>
2024-03-31 10:47:01 +0300

    добавил token и добавил подключение redux-thunk из демо проекта(закомитил ошибка)

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+// import {configureStore} from '@reduxjs/toolkit';
+// import {createAPI} from '../services/api';
+// import { offersSlice } from './slices/slice';
+
+// export const api = createAPI();
+
+// export const store = configureStore({
+//   offersSlice,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       thunk: {
+//         extraArgument: api,
+//       },
+//     }),
+// });
