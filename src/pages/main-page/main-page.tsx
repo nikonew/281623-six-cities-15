9e70ca1dbdcdb0dc86eb4eb681e07097d322db48
nikonew <spiritofadv@yandex.ru>
2024-03-31 09:58:07 +0300

    создал компонент header

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Logo from '../../componets/logo/logo';
 import OfferCard from '../../componets/offer-card/offer-card';
 import {TOffer } from '../../types/types';
 import { Nullable } from 'vitest';
@@ -11,6 +10,7 @@ import { offersSelectors } from '../../store/slices/slice';
 import Sort from '../../componets/sort/sort-main';
 import City from '../../componets/locations-city/locations-city';
 import { sort } from '../../util';
+import Header from '../../componets/header/header';
 
 
 export default function MainPage (): JSX.Element {
@@ -31,36 +31,7 @@ export default function MainPage (): JSX.Element {
 
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                  Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header/>
       <main className={classNames('page__main', 'page__main--index', {'page__main--index-empty': isEmpty})}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
